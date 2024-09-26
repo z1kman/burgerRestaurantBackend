30822a26a790e3532ebc1e1c8270fea786bb228f
anton.shashkov <anton.shashkov@readymag.com>
2024-09-26 19:34:12 +0400

    vercel configuring

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 
 const app = express();
-const port = 4000;
+const PORT = process.env.PORT || 8080;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Started!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Started!");
 });
 
-// Default
-app.get("/api", (req: Request, res: Response) => {
-  res.status(201).json({ message: "Welcome to Auth ts" });
+app.get("/ping", (_req: Request, res: Response) => {
+  res.send("pong 🏓");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
 });
