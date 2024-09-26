9be19ba7ab117fc232eeb7dceef11dc2139f8d7b
anton.shashkov <anton.shashkov@readymag.com>
2024-09-26 19:21:52 +0400

    vercel config updated

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,11 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Started!');
 });
 
+// Default
+app.get("/api", (req: Request, res: Response) => {
+  res.status(201).json({ message: "Welcome to Auth ts" });
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
