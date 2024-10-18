e49a57a7a7f976c9683a8a2d11fae90c75bfa32f
anton.shashkov <anton.shashkov@readymag.com>
2024-10-18 21:19:37 +0400

    updated

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,9 @@
 import { app } from "./app";
-import fs from "fs";
 import https from "https";
-const path = require('path');
 
 const PORT = process.env.PORT || 4000;
 
-const options = {
-  key: fs.readFileSync(path.join(__dirname, '../certs/myserver.key')),
-  cert:  fs.readFileSync(path.join(__dirname, '../certs/myserver.crt'))
-};
 
-https.createServer(options, app).listen(PORT, () => {
-  console.log(`Server is running on port=${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
 });
-
