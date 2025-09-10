91a38ffae707a7d69729d742159143a91b928eb9
Anton Shashkov <anton.shashkov@readymag.com>
2025-09-10 20:27:13 +0400

    updated

--- build.js
+++ build.js
@@ -1,9 +1,21 @@
 import { build } from 'esbuild';
+import fs from 'fs-extra';
+import path from 'path';
 
-build({
+const outDir = path.resolve('dist');
+const prismaSrc = path.resolve('node_modules/.prisma/client');
+const prismaDest = path.join(outDir, 'node_modules/.prisma/client');
+
+await build({
   entryPoints: ['src/server.ts'],
   bundle: true,
   platform: 'node',
   format: 'esm',
-  outfile: 'dist/index.js',
-}).catch(() => process.exit(1));
+  outfile: path.join(outDir, 'index.js'),
+  sourcemap: true,
+});
+
+await fs.ensureDir(path.dirname(prismaDest));
+await fs.copy(prismaSrc, prismaDest);
+
+console.log('✅ Build finished and Prisma client copied!');
