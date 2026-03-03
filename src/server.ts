5861abac0496250e847cc04e82090675304ae9ba
z1kman <z1kmanfast@gmail.com>
2026-03-03 15:42:08 +0000

    updated

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import { app } from "./app";
 const API_PORT = process.env.API_PORT ?? 4000;
 
 
-// app.listen(API_PORT, () => {
-//   console.log(`Server is listening on ${API_PORT}`);
-// });
+app.listen(API_PORT, () => {
+  console.log(`Server is listening on ${API_PORT}`);
+});
